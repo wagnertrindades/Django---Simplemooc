e86424b768cdd524d4882210bbc5cdfbabba28cb
Wagner Trindade <wtrindades@hotmail.com>
2015-07-09 13:04:28 -0300

    Aula 7.1 - View de Cursos

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Course 
 
 def index(resquest):
+	courses = Course.objects.all()
 	template_name = 'courses/index.html'
-	return render(resquest, template_name)
+	context = {
+		'courses' : courses
+	}
+
+	return render(resquest, template_name, context)
