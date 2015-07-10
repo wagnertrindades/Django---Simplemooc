2695d0394eff5ef04b79bf208128daade31d7198
Wagner Trindade <wtrindades@hotmail.com>
2015-07-10 16:33:22 -0300

    Aula 8.1 - Form no template

--- simplemooc/courses/forms.py
+++ simplemooc/courses/forms.py
@@ -5,5 +5,5 @@ class ContactCourse(forms.Form):
     name = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail')
     message = forms.CharField(
-        label='Mensagem/Dúvida', widget=forms.TextArea
-    )     
+        label='Mensagem/Dúvida', widget=forms.Textarea
+    )
--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Course 
+from .models import Course
+from .forms import ContactCourse 
 
 def index(resquest):
 	courses = Course.objects.all()
-	template_name = 'courses/index.html'
 	context = {
 		'courses' : courses
 	}
+	template_name = 'courses/index.html'
 
 	return render(resquest, template_name, context)
 
@@ -23,9 +24,16 @@ def index(resquest):
 # View com url por slug
 def details(resquest, slug):
 	course = get_object_or_404(Course, slug=slug)
-	template_name = 'courses/details.html'
+	# Verifica se o metodo é post ai insere os dados senão retorna vazio
+	if resquest.method == 'POST':
+		form = ContactCourse(resquest.POST)
+	else:
+		form = ContactCourse()
+
 	context = {
-		'course' : course
+		'course' : course,
+		'form' : form
 	}
+	template_name = 'courses/details.html'
 
 	return render(resquest, template_name, context)
