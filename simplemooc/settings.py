7f755cc19b1e44387ec9e30f4bf8756275f1dc53
Wagner Trindade <wtrindades@hotmail.com>
2015-07-12 13:19:54 -0300

    Aula 10.1 - Template de Login

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -103,3 +103,8 @@ EMAIL_HOST_PASSWORD = 'senha'
 EMAIL_PORT = 587
 
 CONTACT_EMAIL = 'wtrindades@hotmail.com'
+
+# Auth
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'core:home'
+LOGOUT_URL = 'accounts:logout'
