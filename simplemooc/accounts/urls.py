6d7096167e5135021c51b58cbb7b3e80d9d7d0df
Wagner Trindade <wtrindades@hotmail.com>
2015-07-12 16:23:46 -0300

    Aula 11.1 - Template do painel do usuário

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'^$', 'simplemooc.accounts.views.dashboard', name='dashboard'),
     url(r'^entrar/$', 'django.contrib.auth.views.login', 
         {'template_name' : 'accounts/login.html'}, name='login'),
     url(r'^sair/$', 'django.contrib.auth.views.logout', 
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import RegisterForm
+
+@login_required
+def dashboard(resquest):
+    template_name = 'accounts/dashboard.html'
+    return render(resquest, template_name)
 
 def register(resquest):
     template_name = 'accounts/register.html'
