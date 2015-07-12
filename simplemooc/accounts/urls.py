1e0ec19caf281f735d3b71bf021950952567d947
Wagner Trindade <wtrindades@hotmail.com>
2015-07-12 14:19:28 -0300

    Aula 10.1 - Form de cadastro

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^entrar/$', 'django.contrib.auth.views.login', 
         {'template_name' : 'accounts/login.html'}, name='login'),
+    url(r'^cadastre-se/$', 'simplemooc.accounts.views.register', name='register'),
 )
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+def register(resquest):
+    template_name = 'accounts/register.html'
+    if resquest.method == 'POST':
+        form = UserCreationForm(resquest.POST)
+        if form.is_valid:
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(resquest, template_name, context)
