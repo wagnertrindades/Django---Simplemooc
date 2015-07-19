5354b548d015760eb173440195c5ae237e474df4
Wagner Trindade <wtrindades@hotmail.com>
2015-07-19 13:31:37 -0300

    Aula 12.1 - Custom User Model

--- simplemooc/accounts/models.py
+++ simplemooc/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Usuário Custom - Herda do AbstractBaseUser e PermissionsMixin assim como o User padrão do django
+
+    A diferença é que aqui configuramos o nosso model diferente do padrão do django,
+    Quando o padrão do django não atende o modelo de usuário utilizamos um custom
+    """
+    username = models.CharField('Nome de Usuário', max_length=30)
+    # Unique é para ser um email unico
+    email = models.EmailField('E-mail', unique=True)
+    name = models.CharField('Nome', max_length=100, blank=True)
+    is_active = models.BooleanField('Está ativo?', blank=True, default=True)
+    is_staff = models.BooleanField('É da equipe?', blank=True, default=False)
+    date_joined = models.DateTimeField('Data de entrada', auto_now_add=True)
+
+    # Adiciona o UserManager que tem coisas uteis para o user
+    objects = UserManager()
+
+    # Indica o campo que é unico e é a referencia na hora de login
+    USERNAME_FIELD = 'username'
+    # É utilizado no comando de criação de superusuário
+    REQUIRED_FIELDS = ['email'] 
+
+    def __str__(self):
+        return self.name or self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return str(self)
+
+    class Meta:
+        verbose_name= 'Usuário'
+        verbose_name_plural= 'Usuários'
