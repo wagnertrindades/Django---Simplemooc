a9a9401c8f0d8e560809d959a82059130ad5620b
Wagner Trindade <wtrindades@hotmail.com>
2015-07-24 15:20:55 -0300

    Aula 17.1 - Modelagem das Aulas

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -78,6 +78,44 @@ class Course(models.Model):
 		verbose_name_plural = 'Cursos'
 		ordering = ['name']
 
+class Lesson(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	description = models.TextField('Descrição', blank=True)
+	number = models.IntegerField('Número (ordem)', blank=True, default=0)
+	release_date = models.DateField('Data de Liberação', blank=True, null=True)
+
+	course = models.ForeignKey(Course, verbose_name='Curso', related_name='lessons')
+
+	created_at = models.DateTimeField('Criado em', auto_now_add=True)
+	updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Aula'
+		verbose_name_plural = 'Aulas'
+		ordering = ['number']
+
+class Material(models.Model):
+
+	name = models.CharField('Nome', max_length=100)
+	embedded = models.TextField('Video embedded', blank=True)
+	archive = models.FileField(upload_to='lessons/materials', blank=True, null=True)
+
+	lesson = models.ForeignKey(Lesson, verbose_name='Aula', related_name='materials')
+
+	def is_embedded(self):
+		return bool(self.embedded)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Material'
+		verbose_name_plural = 'Materiais'
+
 class Enrollment(models.Model):
 
 	STATUS_CHOICES = (
