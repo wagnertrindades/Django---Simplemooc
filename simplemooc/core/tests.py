f73366247d7bded6a2e528edba40978cea66225a
Wagner Trindade <wtrindades@hotmail.com>
2015-07-30 16:58:52 -0300

    Aula 18.1 - Testando Views

--- simplemooc/core/tests.py
+++ simplemooc/core/tests.py
@@ -1,3 +1,17 @@
+from django.core import mail
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+    
+    def test_home_status_code(self):
+        client = Client()
+        response = client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template_used(self):
+        client = Client()
+        response = client.get(reverse('core:home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
