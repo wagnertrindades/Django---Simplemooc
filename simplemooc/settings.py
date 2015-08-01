d79b16d4a6846ef1dc93ad9ce4312776a34a19b7
Wagner Trindade <wtrindades@hotmail.com>
2015-08-01 11:39:59 -0300

    Aula 20.1 - Modelagem do Fórum

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Libs
+    'taggit',
     # My Apps
     'simplemooc.core',
     'simplemooc.accounts',
