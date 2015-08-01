0202ce0b09c673cd8b85e56a9395dff144b0eb69
Wagner Trindade <wtrindades@hotmail.com>
2015-08-01 17:37:36 -0300

    Aula 20.1 - Listagem dos Tópicos 2

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -33,8 +33,21 @@ from .models import Thread
 # ListView
 class ForumView(ListView):
 
-    model = Thread
-    paginate_by = 10
+    paginate_by = 2
     template_name = 'forum/index.html'
+
+    def get_queryset(self):
+        queryset = Thread.objects.all()
+        order = self.request.GET.get('order', '')
+        if order == 'views':
+            queryset = queryset.order_by('-views')
+        elif order == 'answers':
+            queryset = queryset.order_by('-answers')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ForumView, self).get_context_data(**kwargs)
+        context['tags'] = Thread.tags.all()
+        return context
 
 index = ForumView.as_view()
